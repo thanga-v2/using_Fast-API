1492dc4c990f9e5d4639ecbff4ba8c683356c399
thanga-v2 <thangarajm@icloud.com>
2024-09-17 16:22:16 +0530

    fast api setup with endpoints

---
+++ demo.py
@@ -0,0 +1,40 @@
+import os
+from typing import Union
+from fastapi import FastAPI
+from huggingface_hub import InferenceClient
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = InferenceClient(model="gpt2", token=os.getenv('HFTOKEN'))
+app = FastAPI()
+
+
+# print(app)
+
+@app.get("/")
+async def root():
+    return {"message":"hello thanga"}
+
+@app.get("/hello/")
+async def root():
+    time = datetime.now()
+    return {"message":"Once again a hello",
+            "time is": time}
+
+@app.get("/time")
+async def time():
+    time = datetime.now()
+    return {
+        "hey" : "hello, welcome again",
+        "time is" : time
+    }
+
+@app.get("/get_token")
+async def token():
+    token = os.getenv('HFTOKEN')
+    print(token)
+    return {
+        "token - ": token
+    }
